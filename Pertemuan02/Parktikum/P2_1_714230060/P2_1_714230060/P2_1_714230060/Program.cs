e49200d93d9e1c259229c7662cb9d84312a29d34
ferdyleoza <ferdyleoza123@gmail.com>
2024-10-15 16:09:46 +0700

    Praktikum 2

---
+++ Pertemuan02/Parktikum/P2_1_714230060/P2_1_714230060/P2_1_714230060/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230060
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Angka pertama : ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Angka ke dua : ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = input + input2;
+            double hasil2 = input - input2;
+            double hasil3 = input / input2;
+            double hasil4 = input * input2;
+
+            Console.WriteLine(input + "+" + input2 + "=" + hasil);
+            Console.WriteLine(input + "-" + input2 + "=" + hasil2);
+            Console.WriteLine(input + "/" + input2 + "=" + hasil3);
+            Console.WriteLine(input + "*" + input2 + "=" + hasil4);
+
+
+
+        }
+    }
+}
